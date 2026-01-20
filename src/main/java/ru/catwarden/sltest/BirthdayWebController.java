fa482e7f6bf13011f49c0399b5edc2469b49b4da
CatWarden09 <CatWarden09@proton.me>
2026-01-20 22:04:01 +0400

    Add new pages and birthday showing methods
    Added upcoming, today and skipped birthdays showing methods, also added the according html-pages and cross-links in them.

--- src/main/java/ru/catwarden/sltest/BirthdayWebController.java
+++ src/main/java/ru/catwarden/sltest/BirthdayWebController.java
@@ -1,5 +1,6 @@
 package ru.catwarden.sltest;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,27 @@ public class BirthdayWebController {
                 controller.getAllBirthdayList() != null ? controller.getAllBirthdayList() : Collections.emptyList());
 
         return "all_birthdays";
+    }
+
+    @GetMapping("/upcoming")
+    public String showUpcomingBirthdays(Model model){
+        model.addAttribute("upcoming_birthdays",
+                controller.getUpcomingBirthdays() != null ? controller.getUpcomingBirthdays() : Collections.emptyList());
+        return "upcoming_birthdays";
+    }
+
+    @GetMapping("/today")
+    public String showTodayBirthdays(Model model){
+        model.addAttribute("today_birthdays",
+                controller.getTodayBirthdays() != null ? controller.getTodayBirthdays() : Collections.emptyList());
+        return "today_birthdays";
+    }
+
+    @GetMapping("/skipped")
+    public String showSkippedBirthdays(Model model){
+        model.addAttribute("skipped_birthdays",
+            controller.getSkippedBirthdays() != null ? controller.getSkippedBirthdays() : Collections.emptyList());
+        return "skipped_birthdays";
     }
 
 }
